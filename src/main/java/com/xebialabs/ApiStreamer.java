0a3a77d4803b78b856a9912f5b7c7b2f3db91f71
yadavsachin0995 <yadavsachin0995@gmail.com>
2018-05-24 10:58:08 +0530

    Added comments and refractored code

--- src/main/java/com/xebialabs/ApiStreamer.java
+++ src/main/java/com/xebialabs/ApiStreamer.java
@@ -9,7 +9,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ApiStreamer extends Thread {
-
+//    Override the run method to call hitGithubApi() method infinitely
     @Override
     public void run() {
         try {
@@ -22,29 +22,36 @@ public class ApiStreamer extends Thread {
     }
 
     private void hitGithubApi() throws IOException {
-
+//        Username and Password of Github Account
         final String userName = "";
         final String password = "";
 
+//        Create the URL by replacing {USERNAME} with GitHub username and {REPONAME} with any repo
+//        which the account hosts
         URL githubConnect = new URL("https://api.github.com/repos/{USERNAME}/{REPONAME}");
         HttpURLConnection connection = (HttpURLConnection) githubConnect.openConnection();
 
+//        Set HTTP Request Method
         connection.setRequestMethod("GET");
 
+//        Encode the basicAuth Credentials and set it as property
         byte[] encodedPassword = (userName + ":" + password).getBytes();
         BASE64Encoder encoder = new BASE64Encoder();
         connection.setRequestProperty("Authorization",
                 "Basic " + encoder.encode(encodedPassword));
 
+//        Get the input stream from connection into a bufferedReader variable
         BufferedReader bufferedReader = new BufferedReader(
                 new InputStreamReader(
                         connection.getInputStream()));
 
         String line;
 
+//        Write the stream to standard output
         while ((line = bufferedReader.readLine()) != null) {
             System.out.println(line);
         }
+//        Finally close the bufferedReader
         bufferedReader.close();
     }
 }
--- src/main/java/com/xebialabs/LimitExhauster.java
+++ src/main/java/com/xebialabs/LimitExhauster.java
@@ -1,8 +1,8 @@
 package com.xebialabs;
 
 public class LimitExhauster {
-
     public static void main(String[] args) {
+//        Set the number of threads you want to hit the Api
         int numberOfThreads = 50;
         for (int i = 0; i < numberOfThreads; i++) {
             new ApiStreamer().start();
